e43067c19851f9aa0afb640731fe0ef382d5b814
Bndr <Bandar.Obaid.med.s@tuwaiq.edu.sa>
2021-10-18 09:28:43 +0300

    Initial Commit

---
+++ bndarrr11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bndarrr11
+//
+//  Created by بندر عبيد ثاري الرشيدي on 12/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
