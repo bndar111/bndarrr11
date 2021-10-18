71ccefa9bf171c3222575d9cc11547c4963951c7
Bndr <Bandar.Obaid.med.s@tuwaiq.edu.sa>
2021-10-18 09:59:19 +0300

    change§1

--- bndarrr11/ViewController.swift
+++ bndarrr11/ViewController.swift
@@ -3,10 +3,13 @@
 
 
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    var mycoffe = "hot"
+var coofee = "cold"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
